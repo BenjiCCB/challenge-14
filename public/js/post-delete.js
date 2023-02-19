1e7379913f07b2c11fd3ca1da999861858b38f1a
BenjiCCB <bensdecker+CCB@gmail.com>
2023-02-18 21:27:15 -0500

    delete post

---
+++ public/js/post-delete.js
@@ -0,0 +1,22 @@
+const deletePostHandler = async (event) => {
+  
+  console.log("im here")
+  
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete project');
+    }
+  }
+};
+
+document
+  .querySelector('.delete-post')
+  .addEventListener('click', deletePostHandler);
